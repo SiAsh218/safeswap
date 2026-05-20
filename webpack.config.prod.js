f6da12d62dc56fc2799bf9503578ecd67b128e26
SiAsh218 <siashton@live.co.uk>
2026-05-20 10:59:29 +0100

    updated package.json to deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./frontend/src/js/script.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "/frontend/dist"),
+    path: path.resolve(__dirname, "./frontend/dist"),
   },
   mode: "production",
 };
